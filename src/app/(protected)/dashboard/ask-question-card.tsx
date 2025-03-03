b4f59953afe3980764063cd38cb151b005d71503
just-being-aryan <aryansachdevaa@gmail.com>
2025-03-03 20:16:37 +0530

    Succesfully built GitHub RAG Component

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -8,16 +8,36 @@ import { Button } from '@/components/ui/button'
 // import { DialogHeader } from '@/components/ui/dialog'
 import { Dialog, DialogTitle,DialogContent, DialogHeader } from '@/components/ui/dialog'
 import Image from 'next/image'
+import { askQuestion } from './actions'
+import { readStreamableValue } from 'ai/rsc'
 
 const AskQuestionCard = () => {
 
     const {project}  = useProject()
     const [open,setOpen] = React.useState(false)
     const [question,setQuestion] = React.useState('')
+    const [loading,setLoading] = React.useState(false)
+    const [filesReferences,setFilesReferences] = React.useState<{fileName:string;sourceCode:string;summary:string}[]>([])
+    const [answer,setAnswer] = React.useState('')
+
 
     const onSubmit = async(e:React.FormEvent<HTMLFormElement>)  => {
         e.preventDefault()
+        if(!project?.id) return
+        setLoading(true)
         setOpen(true)
+
+        const {output, filesReferences} = await askQuestion(question,project.id)
+        setFilesReferences(filesReferences)
+
+        for await (const delta of readStreamableValue(output)) {
+            if(delta) {
+                setAnswer(ans => ans + delta)
+            }
+        }
+
+        setLoading(false)
+        
 
     }
 
@@ -29,8 +49,12 @@ const AskQuestionCard = () => {
                 <DialogTitle>
                     <Image src ='/logo.png' alt = 'asklogo' width = {100} height = {100}></Image>
                 </DialogTitle>
-                
             </DialogHeader>
+            {answer}
+            <h1>File Reference</h1>
+            {filesReferences.map(file => {
+                return <span>{file.fileName}</span>
+            })}
         </DialogContent>
            
     </Dialog>
