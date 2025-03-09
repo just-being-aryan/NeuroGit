4a865a5b8f47a8eab83f9ad4d5ab91da24d3f1a2
just-being-aryan <aryansachdevaa@gmail.com>
2025-03-09 18:02:10 +0530

    1) Integrated Assembly AI 2)Updated scheme.prisma Issue table to add onCascade support to fix foriegn key violation

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -10,10 +10,18 @@ import { api } from '@/trpc/react';
 import useProject from '@/hooks/use-project';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 const MeetingCard = () => {
 
     const {project} = useProject()
+    const processMeeting = useMutation({
+        mutationFn: async( data: {meetingUrl: string, meetingId: string, projectId: string}) => {
+        const {meetingUrl, meetingId, projectId} = data
+        const response = await axios.post('/api/process-meeting', {meetingUrl,meetingId, projectId})
+        return response.data;
+    }})
     const [isUploading,setIsUploading] = React.useState(false)
     const [progress,setProgress] = React.useState(0)
     const router = useRouter()
@@ -36,9 +44,10 @@ const MeetingCard = () => {
                 meetingUrl: downloadURL,
                 name: file.name
             },{
-                onSuccess: () => {
+                onSuccess: (meeting) => {
                     toast.success('Meeting uploaded Successfully')
                     router.push('/meetings')
+                    processMeeting.mutateAsync({meetingUrl: downloadURL, meetingId: meeting.id, projectId:project.id})
                 },
                 onError: () => {
                     toast.error('Failed to upload Meeting')
--- src/app/(protected)/meetings/page.tsx
+++ src/app/(protected)/meetings/page.tsx
@@ -6,15 +6,18 @@ import MeetingCard from '../dashboard/meeting-card'
 import Link from 'next/link'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
+import useRefetch from '@/hooks/use-refetch'
 
 const MeetingsPage = () => {
 
     const {projectId} = useProject()
     const {data: meetings, isLoading} = api.project.getMeetings.useQuery({projectId},{
         refetchInterval:4000
-    }
-        
-    )
+    })
+
+    const deleteMeeting = api.project.deleteMeeting.useMutation()
+    const refetch = useRefetch()
    
     return (
     <>
@@ -51,10 +54,18 @@ const MeetingsPage = () => {
                     </div>
                     <div className='flex items-center flex-none gap-x-4'>
                             <Link href= {`/meetings/${meeting.id}`}>
-                                <Button variant="outline">
+                                <Button size='sm' variant="outline">
                                     View Meeting
                                 </Button>
                             </Link>
+                            <Button  disabled ={deleteMeeting.isPending} size='sm'  variant='destructive'onClick={() => deleteMeeting.mutate({meetingId:meeting.id}, {
+                               onSuccess: () => {
+                                toast.success('Meeting deleted successfully')
+                                refetch()
+                               }
+                            })}>
+                                Delete Meeting
+                            </Button>
                     </div>
                     
                 </li>
